eba63a7a57c579639b97f797d06360f5b20e31f4
ORTIZJIMENEZANTONIO <joseantonioescom@gmail.com>
2022-11-29 22:26:52 -0600

    npm init

---
+++ index.js
@@ -0,0 +1,31 @@
+// Modules
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+// Variables
+const port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+// app.use();
+// Routes
+app.get("/", (req, res) =>
+  res.json({
+    message: "Welcome to acme back",
+  })
+);
+
+app.listen(port, () => console.log(`Running in port: ${port}`));
+
+// To tests
+module.exports = { app };
