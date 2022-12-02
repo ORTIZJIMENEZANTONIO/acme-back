a519e4301686cd6145b48cba59a8da48e174df3d
ORTIZJIMENEZANTONIO <joseantonioescom@gmail.com>
2022-12-02 11:02:30 -0600

    db-conection fixed

--- db-conection/db-conection.js
+++ db-conection/db-conection.js
@@ -1,11 +1,16 @@
 const mysql = require("mysql");
 
+const getDatabase = () => process.env.MYSQL_DATABASE || "delivery";
+const getUser = () => process.env.MYSQL_USER || "root";
+const getPass = () => process.env.MYSQL_PASSWORD || "root";
+const getHost = () => process.env.MYSQL_HOST || "localhot";
+
 const con = mysql.createPool({
   connectionLimit: 10,
-  host: 'localhot',
-  user: 'root',
-  password: '123',
-  database: 'acme-dev'
+  host: getHost(),
+  user: getUser(),
+  password: getPass(),
+  database: getDatabase(),
 });
 
 module.exports = { con };
